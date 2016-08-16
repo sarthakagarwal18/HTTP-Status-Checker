0de5914f5e90e07898f86541b7e1c460c7c85cb6
Sarthak Agarwal <sarthak18696@gmail.com>
2016-08-16 23:14:27 +0530

    Update urls.py

--- status/urls.py
+++ status/urls.py
@@ -2,6 +2,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^home/', views.home, name='home'),
+    url(r'^$', views.home, name='home'),
     url(r'^status/', views.status, name='status'),
 ]
